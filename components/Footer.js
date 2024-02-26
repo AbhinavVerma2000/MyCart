12cda0bab9b99b1760bb84ba0700a30556468b07
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-26 15:05:23 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -33,6 +33,9 @@ const Footer = () => {
                 <li>
                   <Link href={'/stickers'} className="text-gray-600 hover:text-gray-800">Stickers</Link>
                 </li>
+                <li>
+                  <Link href={'/appliances'} className="text-gray-600 hover:text-gray-800">Appliances</Link>
+                </li>
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
