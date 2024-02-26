73c75f24997aaa92eeda4f3ff6c709ac9f0f80c4
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-27 00:18:31 +0530

    Update mugs.js

--- pages/mugs.js
+++ pages/mugs.js
@@ -1,161 +1,79 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Product from "@/models/Product";
+import mongoose from "mongoose";
+import Link from "next/link";
 
-const Mugs = () => {
+const Mugs = ({ products }) => {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/420x260"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">₹16.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/421x261"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">₹21.15</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/422x262"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">₹12.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/423x263"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">₹18.40</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/424x264"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">₹16.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/425x265"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">₹21.15</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/427x267"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">₹12.00</p>
-              </div>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://dummyimage.com/428x268"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  T-Shirts
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">₹18.40</p>
-              </div>
-            </div>
+            {Object.keys(products).map((item) => {
+              return (
+                <div
+                  key={products[item]._id}
+                  className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5"
+                >
+                  <Link
+                    href={`/product/${products[item].slug}`}
+                    className="block relative h-48 rounded overflow-hidden"
+                  >
+                    <img
+                      alt="ecommerce"
+                      className="object-contain object-center w-full h-full block"
+                      src={products[item].img}
+                    />
+                  </Link>
+                  <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                      {products[item].category}
+                    </h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">
+                      {products[item].title}
+                    </h2>
+                    <p className="mt-1">₹{products[item].price}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
+export const getServerSideProps=(async(context)=> {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find({category: "mugs"});
+  let mugs = {};
+  for (let item of products) {
+    if (item.title in mugs) {
+      if (
+        !mugs[item.title].color.includes(item.color) &&
+        item.availableQty > 0
+      ) {
+        mugs[item.title].color.push(item.color);
+      }
+      if (
+        !mugs[item.title].size.includes(item.size) &&
+        item.availableQty > 0
+      ) {
+        mugs[item.title].size.push(item.size);
+      }
+    } else {
+      mugs[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.availableQty > 0) {
+        mugs[item.title].color = [item.color];
+        mugs[item.title].size = [item.size];
+      }
+    }
+  }
+  return {
+    props: { products: JSON.parse(JSON.stringify(mugs)) },
+  };
+})
 
-export default Mugs
+export default Mugs;
