967fadb800779c46df0165e77dac8a7e2ced4792
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-27 18:04:02 +0530

    Update myaccount.js

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 const MyAccount = () => {
     let router = useRouter()
     useEffect(()=>{
-        if(!localStorage.getItem('token')) router.push('/')
+        if(!localStorage.getItem('token')) router.push(process.env.NEXT_PUBLIC_HOST)
     },[router, router.query])
   return (
     <div>
