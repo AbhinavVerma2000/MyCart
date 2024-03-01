f6fa2a6732d172c17ebfa5f5c9912ae8314f8b57
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-03-01 20:59:52 +0530

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -148,6 +148,29 @@ export default function Page({ addToCart, product, variants }) {
               </h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
+                  {/*<svg
+                    fill="none"
+                    id="1"
+                    onMouseEnter={rating1}
+                    onMouseLeave={rating1}
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-4 h-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <defs>
+                      <linearGradient id="grad">
+                        <stop offset="50%" stop-color="#f97316" />
+                        <stop offset="50%" stop-color="white" />
+                      </linearGradient>
+                    </defs>
+                    <path
+                      fill="url(#grad)"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+                    ></path>
+  </svg>*/}
                   <svg id="1" onMouseEnter={rating1} onMouseLeave={rating1}
                     fill="none"
                     stroke="currentColor"
