229eb9e7f4293249d3dae707b402315fcb0231fb
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-13 18:48:47 +0530

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -551,7 +551,7 @@ export default function Page({ addToCart, product, variants, review }) {
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg></div>
                   
-                  <div className="mt-5 w-60 text-wrap">{item.comment}{console.log(Array.from(review))}</div>
+                  <div className="mt-5 w-60 text-wrap">{item.comment}</div>
                 </div>
               )
             );
