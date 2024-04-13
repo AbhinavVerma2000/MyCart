fc0561736bc4b83b7b3ba79eccdd62ee862a8750
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-13 17:52:07 +0530

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const ReviewSchema = new mongoose.Schema({
     name: {type: String, required:true},
-    email: {type: String, required: true, unique: true},
-    slug: {type: String, required: true, unique: true},
+    email: {type: String, required: true},
+    slug: {type: String, required: true},
     rating: {type: Number, required: true},
     comment:{type: String}
 }, {timestamps: true})
