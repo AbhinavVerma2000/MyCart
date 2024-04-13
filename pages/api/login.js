c5a2f88da70286d1ed16bba4022d20d789d4518e
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-13 14:47:29 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -11,7 +11,7 @@ const handler = async (req, res) => {
     let originalPass = bytes.toString(CryptoJS.enc.Utf8);
     if(user){
         if(req.body.email==user.email && req.body.password==originalPass){
-          var token = jwt.sign({ email: user.email, name: user.name }, 'jwtsecret', {expiresIn: "2d"});
+          var token = jwt.sign({ email: user.email, name: user.name }, 'secret123', {expiresIn: "1d"});
           res.status(200).json({success: true, token,name:user.name});
         }
         else{
